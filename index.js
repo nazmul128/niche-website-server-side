33e27152fe093c6158d2e08c18d31a6e92df8d3e
nazmul128 <shafayat536@gmail.com>
2021-11-11 13:21:35 +0600

    code added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors=require('cors')
-const { MongoClient } = require('mongodb');
+const { MongoClient, Admin } = require('mongodb');
 require('dotenv').config()
  
 const app = express()
@@ -17,7 +17,45 @@ console.log(uri)
 async function run(){
       try{
         await client.connect();
-        console.log('Database connected Successfully')
+       const database=client.db('camera_db')
+       const usersCollection=database.collection('users')
+
+        app.get('/users/:email', async(req,res)=>{
+          const email=req.params.email;
+          const query={email:email};
+          const user=await usersCollection.findOne(query);
+          let isAdmin=false;
+          if(user?.role === 'admin'){
+            isAdmin=true;
+          }
+          res.json({admin:isAdmin});
+        })
+
+
+        app.post('/users', async(req,res)=>{
+          const user=req.body;
+          const result=usersCollection.insertOne(user);
+          console.log(result);
+          res.json(result);
+        });
+        
+        app.put('/users', async(req,res)=>{
+          const user=req.body;
+          const filter={email:user.email};
+          const options={ upsert: true};
+          const updateDoc={ $set:user };
+          const result=await usersCollection.updateOne(filter,updateDoc,options);
+          res.json(result);
+        });
+
+        app.put('/users/admin', async(req,res)=>{
+          const user=req.body;
+          const filter={email:user.email};
+          const updateDoc={$set:{role:'admin'}};
+          const result=await usersCollection.updateOne(filter,updateDoc);
+          res.json(result);
+        })
+
       }
       finally{
         // await client.close();
