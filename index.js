0f73f7bcadb705bdef6c47d80962b9cd2a8cb79f
nazmul128 <shafayat536@gmail.com>
2021-11-13 02:19:10 +0600

    code added

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3ngn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri)
+// console.log(uri)
 
 async function run(){
       try{
         await client.connect();
        const database=client.db('camera_db')
        const usersCollection=database.collection('users')
+       const reviewsCollection=database.collection('reviews')
+       const productsCollection=database.collection('products')
+       const addUserCollection=database.collection('addUser')
 
         app.get('/users/:email', async(req,res)=>{
           const email=req.params.email;
@@ -32,12 +35,47 @@ async function run(){
         })
 
 
+        //get products api
+        app.get('/products', async(req,res)=>{
+          const cursor = productsCollection.find({});
+          const products = await cursor.toArray();
+          res.send(products);
+        });
+
+
+        //get single product
+        app.get('/products/:id', async (req,res)=>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const result = await productsCollection.findOne(query);
+           res.json(result);
+        });
+
+
+        //add a product
+        app.post('/products', async(req,res)=>{
+          const products=req.body;
+          const result=await productsCollection.insertOne(products);
+          // console.log(result)
+          res.json(result);
+        });
+
+      
+
         app.post('/users', async(req,res)=>{
           const user=req.body;
           const result=usersCollection.insertOne(user);
-          console.log(result);
+          // console.log(result);
           res.json(result);
         });
+        
+           //reviews
+           app.post("/reviews", async (req, res) => {
+            const review=(req.body);
+            const result = await reviewsCollection.insertOne(review);
+            // console.log(result)
+            res.json(result);
+          });
         
         app.put('/users', async(req,res)=>{
           const user=req.body;
@@ -54,7 +92,18 @@ async function run(){
           const updateDoc={$set:{role:'admin'}};
           const result=await usersCollection.updateOne(filter,updateDoc);
           res.json(result);
-        })
+        });
+
+         //add a user and display myOrder
+         app.post('/addUser', async(req,res)=>{
+          const addUsers=req.body;
+          const result=await addUserCollection.insertOne(addUsers);
+          console.log(result)
+          res.json(result);
+        });
+
+     
+
 
       }
       finally{
